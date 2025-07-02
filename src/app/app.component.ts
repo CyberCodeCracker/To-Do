ac80188502968768041b4aa1b550b18afb0b8770
CyberCodeCracker <souhailamouri123@gmail.com>
2025-07-02 10:54:12 +0100

    Initial configuration and making user and header components

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
+
+import { DUMMY_USERS } from './dummy-users';
+
+@Component({
+  selector: 'app-root',
+  standalone: true, 
+  imports: [HeaderComponent, UserComponent], 
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+}
